9a2dad9bdbf5c0596dcfe66307796a0a7246c3a9
Ivan-Andrusyshyn <115649537+Ivan-Andrusyshyn@users.noreply.github.com>
2023-04-24 15:36:39 +0000

    Deploying to gh-pages from @ Ivan-Andrusyshyn/Boostrap2@271a4d905564e6baa4651915dd1a2e928a211fa7 🚀

---
+++ html/search.36c457aa.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[btn-active-add]");t.addEventListener("click",(function(){console.log("Hello"),"Add"==t.textContent?t.textContent="delete":t.textContent="Add"}))}();
+//# sourceMappingURL=search.36c457aa.js.map
---
+++ html/search.9a8abb56.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[btn-active-add]");t.addEventListener("click",(function(){console.log("Hello"),"Add"==t.textContent?t.textContent="delete":t.textContent="Add"}));
+//# sourceMappingURL=search.9a8abb56.js.map
